f9e61eea04dfbcabcbefae7ec07eaa758e46d58a
Pratyush Kumar Jaiswal <pratyush.jaiswal2525@gmail.com>
2024-06-20 10:32:22 +0530

    Basic Authentication

---
+++ Spring_Security_Basic_Auth/src/main/java/com/gl/Spring_Security_Basic_Auth/SpringSecurityBasicAuthApplication.java
@@ -0,0 +1,13 @@
+package com.gl.Spring_Security_Basic_Auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBasicAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityBasicAuthApplication.class, args);
+	}
+
+}
---
+++ Spring_Security_Basic_Auth/src/test/java/com/gl/Spring_Security_Basic_Auth/SpringSecurityBasicAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gl.Spring_Security_Basic_Auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBasicAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
