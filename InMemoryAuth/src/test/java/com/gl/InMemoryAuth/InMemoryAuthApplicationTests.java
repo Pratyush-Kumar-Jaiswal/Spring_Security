a9ed37fe8b9958b73e3a22129b2e6bbcedc05bb0
Pratyush Kumar Jaiswal <pratyush.jaiswal2525@gmail.com>
2024-06-21 10:40:18 +0530

    In Memory Authentication

---
+++ InMemoryAuth/src/test/java/com/gl/InMemoryAuth/InMemoryAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gl.InMemoryAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InMemoryAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
