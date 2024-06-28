9298248707d0f48428f57e00e8572779baca3b25
Pratyush Kumar Jaiswal <pratyush.jaiswal2525@gmail.com>
2024-06-28 10:04:39 +0530

    H2 Database

---
+++ H2Database/src/main/java/com/gl/H2Database/GreetingsController.java
@@ -0,0 +1,26 @@
+package com.gl.H2Database;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hiii";
+    }
+
+    @PreAuthorize("hasRole('user')")
+    @GetMapping("/user")
+    public String sayHelloUser(){
+        return "Hello User";
+    }
+
+    @PreAuthorize("hasRole('admin')")
+    @GetMapping("/admin")
+    public String sayHelloAdmin(){
+        return "Hello Admin";
+    }
+}
